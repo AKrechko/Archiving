c9817cbb743b9688610b5d051bf23eb06fa9cde6
Alexander <45324268+AKrechko@users.noreply.github.com>
2021-02-01 11:03:15 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-# Сообщить за сколько времени прошла архивация
-TIMEFORMAT="время выполнения %lR"
+TIMEFORMAT="время выполнения %lR" # Сообщить за сколько времени прошла архивация
 time {
 CURRENTDATE=`date +"%d%m%Y"`
-# Путь до папки какую архивируем
-SOURCE_DIR='/home/user/'
-# Путь где сохранить архив
-TARGET_DIR='/media/ssd/Data/'
-# Имя архива
-FILE_NAME=${TARGET_DIR}${CURRENTDATE}.tar
-# Начало архивации
+SOURCE_DIR='/home/user/' # Путь до папки какую архивируем
+TARGET_DIR='/media/ssd/Data/' # Путь где сохранить архив
+FILE_NAME=${TARGET_DIR}${CURRENTDATE}.tar # Имя архива
 tar --exclude="/home/user/.cache" --exclude="/home/user/VirtualBox VMs" --exclude="/home/user/.local/share/Steam" -cf ${FILE_NAME} ${SOURCE_DIR}
 }
