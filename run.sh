bcffd479ab26258bba34f285f467dd66eff1ae34
Alexander <45324268+AKrechko@users.noreply.github.com>
2021-02-09 16:53:27 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ CURRENTDATE=`date +"%d%m%Y"`
 SOURCE_DIR='/home/user/' # Путь до папки какую архивируем
 TARGET_DIR='/media/ssd/Data/' # Путь где сохранить архив
 FILE_NAME=${TARGET_DIR}${CURRENTDATE}.tar # Имя архива
-tar --exclude="/home/user/.cache" --exclude="/home/user/.local/share/Steam" -cf ${FILE_NAME} ${SOURCE_DIR}
+tar --exclude="/home/user/.cache" --exclude="/home/user/.local/share/Steam" -cvf ${FILE_NAME} ${SOURCE_DIR}
